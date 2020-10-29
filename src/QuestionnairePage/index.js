8e5ee6ff6d73833761f6af4ffe38d7cc4b0eff78
janimaenpaa <itsmayd@gmail.com>
2020-10-29 09:37:10 +0200

    Add QuestionnairePage

---
+++ src/QuestionnairePage/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Container from "../components/Container"
+import Card from "../components/Card"
+ 
+const QuestionnairePage = () => {
+    return (
+        <Container>
+            <Card>Tiikeri</Card>
+        </Container>
+    )
+}
+
+export default QuestionnairePage
