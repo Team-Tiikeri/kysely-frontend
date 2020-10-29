5a925f2d954141df6913e63847c29c3fb96d3a94
janimaenpaa <itsmayd@gmail.com>
2020-10-29 09:24:41 +0200

    Add Card component

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from "react"
+import MaterialUICard from "@material-ui/core/Card"
+import CardContent from "@material-ui/core/CardContent"
+
+const Card = ({ children, styles }) => {
+  const customStyle = {
+    ...styles,
+    minWidth: 275,
+  }
+
+  return (
+    <MaterialUICard className={customStyle} variant="outlined">
+      <CardContent>{children}</CardContent>
+    </MaterialUICard>
+  )
+}
+
+export default Card
