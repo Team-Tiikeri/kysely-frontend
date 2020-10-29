b074a41aad5c5a7a9b3e96434d8477183d0b7185
janimaenpaa <itsmayd@gmail.com>
2020-10-29 11:00:35 +0200

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react"
 import "./App.css"
+import NavBar from "./components/NavBar"
+import QuestionnairePage from "./QuestionnairePage"
 
 const App = () => {
-  return <div className="App">Hello world!</div>
+  return (
+    <div className="App">
+      <NavBar />
+      <QuestionnairePage />
+    </div>
+  )
 }
 
 export default App
