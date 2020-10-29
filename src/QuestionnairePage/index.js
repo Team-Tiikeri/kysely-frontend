6f321c41ea9e1faeb828cbc52fad7898d18a8c5f
janimaenpaa <itsmayd@gmail.com>
2020-10-29 11:00:18 +0200

    Edit QuestionnairePage to show questionnaires from mock data

--- src/QuestionnairePage/index.js
+++ src/QuestionnairePage/index.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react"
 import Container from "../components/Container"
 import Card from "../components/Card"
- 
+import data from "../mockData.json"
+
 const QuestionnairePage = () => {
-    return (
-        <Container>
-            <Card>Tiikeri</Card>
-        </Container>
-    )
+  return (
+    <Container>
+      <Card>
+        {data.questionnaires.map((questionnaire) => (
+          <div
+            key={questionnaire.id}
+          >{`ID: ${questionnaire.id} - ${questionnaire.title} - Questions: ${questionnaire.questions.length}`}</div>
+        ))}
+      </Card>
+    </Container>
+  )
 }
 
 export default QuestionnairePage
