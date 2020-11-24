de18780d057c19fffbd62cada8b9d4162b1e482d
janimaenpaa <itsmayd@gmail.com>
2020-11-24 14:14:41 +0200

    Add ReportList page

---
+++ src/ReportList/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Card from "../components/Card"
+import Container from "../components/Container"
+
+const ReportList = () => {
+  return (
+    <Container>
+      <Card>Reports</Card>
+    </Container>
+  )
+}
+
+export default ReportList
