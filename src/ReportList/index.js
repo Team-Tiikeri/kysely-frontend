ab63644fb671aedbebdf48a51a13de43d3ff149b
janimaenpaa <itsmayd@gmail.com>
2020-11-24 14:27:12 +0200

    Add Questionnaire links to their report pages

--- src/ReportList/index.js
+++ src/ReportList/index.js
@@ -1,11 +1,27 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import Card from "../components/Card"
 import Container from "../components/Container"
 
-const ReportList = () => {
+const Questionnaire = ({ questionnaire }) => (
+  <li key={questionnaire.questionnaireId}>
+    <Link to={`/reports/${questionnaire.questionnaireId}`}>
+      {questionnaire.title}
+    </Link>
+  </li>
+)
+
+const ReportList = ({ data }) => {
   return (
     <Container>
-      <Card>Reports</Card>
+      <h2>Reports</h2>
+      <Card>
+        <ul>
+          {data.map((questionnaire) => (
+            <Questionnaire questionnaire={questionnaire} />
+          ))}
+        </ul>
+      </Card>
     </Container>
   )
 }
