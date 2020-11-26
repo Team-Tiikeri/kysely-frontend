10b3df963104009f7c7efb5aa923b45102682621
Tuukka Mattila <tuukka.mattila@myy.haaga-helia.fi>
2020-11-26 12:26:26 +0200

    Display questionnaire data on report page

--- src/ReportPage/index.js
+++ src/ReportPage/index.js
@@ -1,24 +1,41 @@
-import React, { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import Card from "../components/Card"
-import Container from "../components/Container"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Card from "../components/Card";
+import Container from "../components/Container";
 
 const ReportPage = () => {
-  const [questionnaire, setQuestionnaire] = useState({})
-  const { id } = useParams()
+  const [questionnaire, setQuestionnaire] = useState(null);
+  const { id } = useParams();
 
   useEffect(() => {
     fetch(`https://ohp20kysely.herokuapp.com/api/questionnaires/${id}`)
       .then((response) => response.json())
       .then((response) => setQuestionnaire(response))
-      .catch((err) => console.log(err))
-  }, [id])
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  if (!questionnaire) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <Container>
-      <Card>{questionnaire.title}</Card>
-    </Container>
-  )
-}
+      <Card>
+        {questionnaire.title}
 
-export default ReportPage
+        {Object.values(questionnaire.questions).map((question, index) => (
+          <div key={index}>
+            <h3> {question.content}</h3>
+            <ul>
+              {question.answers.map((answer, index) => (
+                <li key={index}>{answer.content}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </Card>
+    </Container>
+  );
+};
+
+export default ReportPage;
